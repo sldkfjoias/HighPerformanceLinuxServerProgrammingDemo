cbc944ab88f0596a560a6ea5e4031d6c3650c279
Sldkfjoias <sldkfjoias@gmail.com>
2021-05-26 23:34:02 +0800

    5.26

---
+++ Chapter-11-timeOutConnect.cpp
@@ -0,0 +1,55 @@
+//set time out connect
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<assert.h>
+#include<errno.h>
+#include <iostream>
+
+int timeout_connect(const char* ip, int port, int time){
+    struct sockaddr_in address;
+    bzero(&address, sizeof(address));
+    address.sin_family = AF_INET;
+    inet_pton(AF_INET, ip, &address.sin_addr);
+    address.sin_port = htons(port);
+
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    assert(sockfd >= 0);
+
+    struct timeval timeout;
+    timeout.tv_sec = time;
+    timeout.tv_usec = 0;
+    socklen_t len = sizeof(timeout);
+    int ret = setsockopt(sockfd, SOL_SOCKET, SO_SNDTIMEO, &timeout, len);
+    assert(ret != -1);
+
+    ret = connect(sockfd, (struct sockaddr*)&address, sizeof(address));
+    std::cout<<ret;
+    if(ret == -1){
+        std::cout<<errno<<std::endl;
+        if(errno == EINPROGRESS){
+            printf("connecting time out, process timeout logic\n");
+            return -1;
+        }
+        printf("ERROR!\n");
+        return -1;
+    }
+    return sockfd;
+}
+
+int main(int argc, char* argv[]){
+    assert(argc > 2);
+    const char* ip = argv[1];
+    int port = atoi(argv[2]);
+
+    int sockfd = timeout_connect(ip, port, 10);
+    std::cout<<sockfd<<std::endl;
+    if(sockfd < 0){
+        return 1;
+    }
+    return 0;
+}
